eab9a93ad45dad56ac93ff666e12d067672307ca
Adam Robson <adamray312@gmail.com>
2023-01-06 09:16:24 -0800

    add blogs to services

---
+++ src/services/blogs.jsx
@@ -0,0 +1,6 @@
+import { checkError, client } from './client';
+
+export async function getBlogs() {
+  const res = await client.from('blogs').select(`*, authors (name)`);
+  return checkError(res);
+}
